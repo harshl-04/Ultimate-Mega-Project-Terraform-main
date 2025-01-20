708acc51b6a8d9831ca72a5e3620ca42aa6a59e3
harshl-04 <89849384+harshl-04@users.noreply.github.com>
2025-01-20 18:09:03 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.harsh.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.harsh.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.harsh_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.harsh_subnet[*].id
 }
